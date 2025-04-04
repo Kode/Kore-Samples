8f9d034c48be4fc02464fb12bfc7e531f619079a
Robert Konrad <git2016@robdangero.us>
2025-04-05 01:42:01 +0200

    Make screenshot.h work with emscripten

--- screenshot.h
+++ screenshot.h
@@ -3,6 +3,34 @@
 
 #include <kore3/util/align.h>
 
+#ifdef KORE_EMSCRIPTEN
+#include <emscripten.h>
+
+static uint8_t *png_data = NULL;
+static int png_data_size = 0;
+
+static void png_write_func(void *context, void *data, int size) {
+	memcpy(&png_data[png_data_size], data, size);
+	png_data_size += size;
+}
+
+EM_JS(void, kore_html5_download_image, (const char *filename, void *data, int size), {
+	const a = document.createElement('a');
+	a.style = 'display:none';
+	document.body.appendChild(a);
+	const view = new Uint8Array(Module.HEAPU8.buffer, data, size);
+	const blob = new Blob([view], {
+		type: 'octet/stream'
+	});
+	const url = window.URL.createObjectURL(blob);
+	a.href = url;
+	a.download = UTF8ToString(filename);
+	a.click();
+	window.URL.revokeObjectURL(url);
+	document.body.removeChild(a);
+});
+#endif
+
 static kore_gpu_buffer screenshot_buffer;
 
 static void screenshot_take(kore_gpu_device *device, kore_gpu_command_list *list, kore_gpu_texture *framebuffer, uint32_t width, uint32_t height) {
@@ -10,7 +38,7 @@ static void screenshot_take(kore_gpu_device *device, kore_gpu_command_list *list
 
 	kore_gpu_buffer_parameters buffer_parameters = {
 	    .size        = row_bytes * height,
-	    .usage_flags = KORE_GPU_BUFFER_USAGE_CPU_READ,
+	    .usage_flags = KORE_GPU_BUFFER_USAGE_CPU_READ | KORE_GPU_BUFFER_USAGE_COPY_DST,
 	};
 	kore_gpu_device_create_buffer(device, &buffer_parameters, &screenshot_buffer);
 
@@ -47,9 +75,20 @@ static void screenshot_take(kore_gpu_device *device, kore_gpu_command_list *list
 		}
 	}
 
+#ifdef KORE_EMSCRIPTEN
+	png_data = (uint8_t *)malloc(width * height * 4);
+	png_data_size = 0;
+	stbi_write_png_to_func(png_write_func, NULL, width, height, 4, pixels, row_bytes);
+	kore_html5_download_image("test.png", png_data, png_data_size);
+#else
 	stbi_write_png("test.png", width, height, 4, pixels, row_bytes);
+#endif
 
 	kore_gpu_buffer_unlock(&screenshot_buffer);
 
+#ifdef KORE_EMSCRIPTEN
+	emscripten_force_exit(0);
+#else
 	exit(0);
+#endif
 }
