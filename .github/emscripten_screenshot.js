aabf3b0e077d02360f8be384e8150a40abcc720b
Robert Konrad <git2016@robdangero.us>
2025-04-05 02:12:28 +0200

    Add a script to create Emscripten screenshots

---
+++ .github/emscripten_screenshot.js
@@ -0,0 +1,38 @@
+const puppeteer = require("puppeteer");
+const nstatic = require('node-static');
+const path = require('path');
+
+console.log('Running server on 8888');
+const fileServer = new nstatic.Server(path.join(__dirname, 'build', 'debug'), { cache: 0 });
+
+const server = require('http').createServer((request, response) => {
+	request.addListener('end', function () {
+		fileServer.serve(request, response);
+	}).resume();
+});
+
+server.on('error', (e) => {
+	if (e.code === 'EADDRINUSE') {
+		console.log('Error: Port 8888 is already in use.');
+	}
+});
+
+server.listen(8888);
+
+(async () => {
+	const browser = await puppeteer.launch();
+	const page = await browser.newPage();
+
+	await page.goto('http://localhost:8888');
+
+	const client = await page.target().createCDPSession();
+	await client.send('Page.setDownloadBehavior', {
+		behavior: 'allow',
+		downloadPath: __dirname,
+	});
+
+	setTimeout(async () => {
+		await browser.close();
+		server.close();
+	}, 5000);
+})();
