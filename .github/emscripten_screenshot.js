b22558ac5ebe36917dd0f7556986a360c95c8e0f
Robert Konrad <git2016@robdangero.us>
2025-04-05 03:26:03 +0200

    Fix logging, maybe

--- .github/emscripten_screenshot.js
+++ .github/emscripten_screenshot.js
@@ -1,10 +1,11 @@
 const puppeteer = require("puppeteer");
 const nstatic = require('node-static');
+const core = require('@actions/core');
 
 const fs = require('fs');
 const path = require('path');
 
-fs.appendFileSync(process.env.GITHUB_OUTPUT, 'Running server on 8888');
+core.info('Running server on 8888');
 const fileServer = new nstatic.Server(path.join(process.cwd(), 'build', 'debug'), { cache: 0 });
 
 const server = require('http').createServer((request, response) => {
@@ -15,13 +16,13 @@ const server = require('http').createServer((request, response) => {
 
 server.on('error', (e) => {
 	if (e.code === 'EADDRINUSE') {
-		fs.appendFileSync(process.env.GITHUB_OUTPUT, 'Error: Port 8888 is already in use.');
+		core.info('Error: Port 8888 is already in use.');
 	}
 });
 
 server.listen(8888);
 
-fs.appendFileSync(process.env.GITHUB_OUTPUT, 'Starting browser');
+core.info('Starting browser');
 (async () => {
 	const browser = await puppeteer.launch({args: ['--no-sandbox']});
 	const page = await browser.newPage();
@@ -37,6 +38,6 @@ fs.appendFileSync(process.env.GITHUB_OUTPUT, 'Starting browser');
 	setTimeout(async () => {
 		await browser.close();
 		server.close();
-		fs.appendFileSync(process.env.GITHUB_OUTPUT, 'Browser and server closed');
+		core.info('Browser and server closed');
 	}, 5000);
 })();
