3dfe396c11dca6173e86b5e6c6b31acffee3b4b2
Robert Konrad <git2016@robdangero.us>
2025-06-16 03:05:11 +0200

    Set max_anisotropy in 13

--- 13_generate_mipmaps/sources/main.c
+++ 13_generate_mipmaps/sources/main.c
@@ -159,8 +159,9 @@ int kickstart(int argc, char **argv) {
 	}
 
 	kore_gpu_sampler_parameters sampler_params = {
-	    .lod_min_clamp = 0,
-	    .lod_max_clamp = 5,
+	    .lod_min_clamp  = 0,
+	    .lod_max_clamp  = 5,
+	    .max_anisotropy = 1,
 	};
 	kore_gpu_device_create_sampler(&device, &sampler_params, &sampler);
 
