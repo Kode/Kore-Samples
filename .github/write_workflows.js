75d2c996fbda03be648cc3b3cbae5657a5839808
Robert Konrad <git2016@robdangero.us>
2025-07-07 02:37:20 +0200

    Check more Vulkan

--- .github/write_workflows.js
+++ .github/write_workflows.js
@@ -320,7 +320,7 @@ const workflows = [
     runsOn: 'ubuntu-latest',
     canExecute: true,
     xvfb: true,
-    checked: [1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,0,0,0,0,0],
+    checked: [1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,0,0,0,0,1],
     steps:
 `    - name: Get LunarG key
       run: wget -qO- https://packages.lunarg.com/lunarg-signing-key-pub.asc | sudo tee /etc/apt/trusted.gpg.d/lunarg.asc
@@ -398,7 +398,7 @@ const workflows = [
           Invoke-WebRequest -Uri "https://sdk.lunarg.com/sdk/download/1.3.275.0/windows/VulkanSDK-1.3.275.0-Installer.exe" -OutFile VulkanSDK.exe
           $installer = Start-Process -FilePath VulkanSDK.exe -Wait -PassThru -ArgumentList @("--da", "--al", "-c", "in");
           $installer.WaitForExit();`,
-    checked: [1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,0,0,0,0,0]
+    checked: [1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,0,0,0,0,1]
   }
 ];
 
